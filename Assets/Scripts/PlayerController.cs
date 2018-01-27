58ec73515d176e17c53c39c347a01ff823ad1fe2
Demetrio <thepreside93@gmail.com>
2018-01-27 01:48:43 +0100

    Primo commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public int player;
+    public float speed = 1;
+    public float railLength = 10;
+    public float angle = 0;
+    public Projectile projectilePrefab;
+
+    private GameManager.Types shield = GameManager.Types.Green;
+    private string inputStr = "";
+    private SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.green;
+        if (player == 2) { speed *= -1; }
+        inputStr = "P" + player;
+    }
+
+    void Update() {
+        Move();
+        SetShield();
+        Fire();
+    }
+
+    private void Move() {
+        float yPos = Mathf.Sin(angle) * railLength / 2f;
+        transform.position = new Vector3(transform.position.x, yPos, 0);
+        angle += Time.deltaTime * speed;
+        if (Mathf.Abs(angle) >= Mathf.PI * 2) { angle = 0; }
+    }
+
+    private void SetShield() {
+        GameManager.Types type;
+        if (Input.GetButtonDown(inputStr + "ShieldGreen")) { type = GameManager.Types.Green; }
+        else if (Input.GetButtonDown(inputStr + "ShieldRed")) { type = GameManager.Types.Red; }
+        else if (Input.GetButtonDown(inputStr + "ShieldBlue")) { type = GameManager.Types.Blue; }
+        else if (Input.GetButtonDown(inputStr + "ShieldYellow")) { type = GameManager.Types.Yellow; }
+        else { return; }
+        shield = type;
+        switch (type) {
+            case GameManager.Types.Green:
+                spriteRenderer.color = Color.green;
+                break;
+            case GameManager.Types.Red:
+                spriteRenderer.color = Color.red;
+                break;
+            case GameManager.Types.Blue:
+                spriteRenderer.color = Color.blue;
+                break;
+            case GameManager.Types.Yellow:
+                spriteRenderer.color = Color.yellow;
+                break;
+        }
+    }
+
+    private void Fire() {
+        GameManager.Types type;
+        if (Input.GetButtonDown(inputStr + "FireGreen")) { type = GameManager.Types.Green; }
+        else if (Input.GetButtonDown(inputStr + "FireRed")) { type = GameManager.Types.Red; }
+        else if (Input.GetButtonDown(inputStr + "FireBlue")) { type = GameManager.Types.Blue; }
+        else if (Input.GetButtonDown(inputStr + "FireYellow")) { type = GameManager.Types.Yellow; }
+        else { return; }
+        Projectile projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity) as Projectile;
+        projectile.Initialize(player == 1 ? 1 : -1, type);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed = 3;
+
+    private int direction = 1;
+    private GameManager.Types type;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+        transform.Translate(new Vector3(speed * direction * Time.deltaTime, 0, 0));
+    }
+
+    public void Initialize(int direction, GameManager.Types type) {
+        this.direction = direction;
+        this.type = type;
+        switch (type) {
+            case GameManager.Types.Green:
+                spriteRenderer.color = Color.green;
+                break;
+            case GameManager.Types.Red:
+                spriteRenderer.color = Color.red;
+                break;
+            case GameManager.Types.Blue:
+                spriteRenderer.color = Color.blue;
+                break;
+            case GameManager.Types.Yellow:
+                spriteRenderer.color = Color.yellow;
+                break;
+        }
+    }
+}
