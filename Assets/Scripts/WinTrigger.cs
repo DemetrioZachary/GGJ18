47f291185217b8056d5e2652254054785031d6b1
Demetrio <thepreside93@gmail.com>
2018-01-28 16:30:32 +0100

    g

--- Assets/Scripts/WinTrigger.cs
+++ Assets/Scripts/WinTrigger.cs
@@ -5,12 +5,9 @@ using UnityEngine.UI;
 
 public class WinTrigger : MonoBehaviour {
 
-    public Text winnerText;
-
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
-            FindObjectOfType<GameManager>().DeclareWinner();
-            winnerText.text = collision.gameObject.name.ToUpper() + "WINS";
+            
         }
     }
 }
